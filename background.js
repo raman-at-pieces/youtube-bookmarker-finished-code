868e51abca080637171b818fcf76aad7ad43ab04
知止 <boxoft@gmail.com>
2023-04-03 10:37:33 +0800

    Fix: Chrome extension now properly handles tab updates and activations
    This commit fixes the bug where the Chrome extension did not work
    for certain video URLs. The solution involves using both
    chrome.tabs.onUpdated and chrome.tabs.onActivated event listeners in
    background.js to ensure the content script receives the necessary
    messages.
    
    - Added chrome.tabs.onActivated event listener with chrome.tabs.get method
    - Updated chrome.tabs.onUpdated event listener to check for 'complete' status

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
+/*
+GPT-4: This code will listen for both chrome.tabs.onUpdated and chrome.tabs.onActivated events. When a tab is updated, it will check if the update is complete before sending the message. When a tab is activated, it will use chrome.tabs.get to retrieve the current tab information and send the message accordingly.
+
+This should ensure that the message is being sent to contentScript.js when the URL is updated, and also when you switch between different tabs.
+*/
+
+const sendMessageToContentScript = (tabId, url) => {
+  if (url && url.includes("youtube.com/watch")) {
+    const queryParameters = url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
 
     chrome.tabs.sendMessage(tabId, {
@@ -8,4 +14,16 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
       videoId: urlParameters.get("v"),
     });
   }
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    sendMessageToContentScript(tabId, tab.url);
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    sendMessageToContentScript(activeInfo.tabId, tab.url);
+  });
 });
