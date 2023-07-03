a051fea66dc5992096e31e9c7c92d7a0fca0dfb6
cnm13ryan <channokman16@gmail.com>
2023-07-03 21:27:41 +0100

    fix: Modify background.js
        Continue to fix on top of (https://github.com/raman-at-pieces/youtube-bookmarker-finished-code/pull/5)
        Added error checking that prints the error message to the console

--- background.js
+++ background.js
@@ -1,11 +1,29 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
+const sendMessageToContentScript = (tabId, url) => {
+  if (url && url.includes("youtube.com/watch")) {
+    const queryParameters = url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
 
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
       videoId: urlParameters.get("v"),
+    }, function(response) {
+      if (chrome.runtime.lastError) {
+        console.log(`Error: ${chrome.runtime.lastError.message}`);
+      } else {
+        console.log(`Received response: ${response}`);
+      }
     });
   }
+};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    sendMessageToContentScript(tabId, tab.url);
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    sendMessageToContentScript(activeInfo.tabId, tab.url);
+  });
 });
